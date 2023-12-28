beb3ea778cb08a15b636b3cd72874929d6e56ad3
kenan551 <kenan.ramazanov1104@gmail.com>
2023-12-28 13:30:28 +0400

    Creating SocialMedia Buttons

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -5,6 +5,8 @@ import Input from "@/app/components/inputs/Input";
 import { useRouter } from "next/navigation";
 import { useCallback, useState } from "react"
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { BsGithub, BsGoogle  } from 'react-icons/bs';
+import AuthSocialButton from "./AuthSocialButton";
 type Variant = 'LOGIN' | 'REGISTER';
 const AuthForm = () => {
     const [variant,setVariant] = useState<Variant>('LOGIN');
@@ -41,7 +43,7 @@ const AuthForm = () => {
             // NextAuth SignIn
         }
     }
-    const sosialAction = (action: string) => {
+    const socialAction = (action: string) => {
         setIsLoading(true);
 
         // NextAuth SocialSign
@@ -90,11 +92,68 @@ const AuthForm = () => {
        register={register}
        />
        <div>
-        <Button>
-            Test
+        <Button
+        disabled={isLoading}
+        fullWidth
+        type="submit"
+        >
+            {variant === 'LOGIN' ? 'Sign in' : 'Register'}
         </Button>
        </div>
        </form>
+
+       <div className="mt-6">
+         <div className="relative">
+            <div className="
+            absolute 
+            inset-0 
+            flex
+             items-center">
+              <div 
+              className="
+              w-full
+              border-t
+             border-gray-300" 
+             />
+            </div>
+            <div className="relative flex justify-center text-sm">
+              <span className=" bg-white px-2 text-gray-500">
+              Or continue with
+              </span>
+            </div>
+         </div>
+         <div className="mt-6 flex gap-2">
+          <AuthSocialButton
+          icon={BsGithub} 
+          onClick={() => socialAction('github')} 
+          />
+           <AuthSocialButton 
+              icon={BsGoogle} 
+              onClick={() => socialAction('google')} 
+            />
+         </div>
+       </div>
+       <div 
+          className="
+            flex 
+            gap-2 
+            justify-center 
+            text-sm 
+            mt-6 
+            px-2 
+            text-gray-500
+          "
+        >
+          <div>
+            {variant === 'LOGIN' ? 'New to Messenger?' : 'Already have an account?'} 
+          </div>
+          <div 
+            onClick={toggleVariant} 
+            className="underline cursor-pointer"
+          >
+            {variant === 'LOGIN' ? 'Create an account' : 'Login'}
+          </div>
+        </div>
         </div>
     </div>
   )
