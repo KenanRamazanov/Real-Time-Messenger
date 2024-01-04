b1ce0fc512df1f5fd77f6bf57c8da7344e56d73e
kenan551 <kenan.ramazanov1104@gmail.com>
2024-01-05 00:24:35 +0400

    Creating Avatar

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -2,10 +2,19 @@
 import useRoutes from '@/app/hooks/useRoutes'
 import React, { useState } from 'react'
 import DesktopItem from './DesktopItem';
+import { User } from '@prisma/client';
+import Avatar from '@/app/(site)/components/Avatar';
 
-const DesktopSidebar = () => {
+
+interface DesktopSidebarProps {
+  currentUser:  User
+}
+const DesktopSidebar:React.FC<DesktopSidebarProps> = ({
+  currentUser
+}) => {
     const routes = useRoutes();
-    const [isOpen, setisOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
+    console.log(currentUser)
   return (
     <div 
     className='
@@ -38,6 +47,20 @@ const DesktopSidebar = () => {
               />
             ))}
           </ul>
+</nav>
+<nav 
+className='
+mt-4 
+flex 
+flex-col
+ justify-between
+  items-center'>
+  <div 
+  onClick={() => setIsOpen(true)}
+  className="cursor-pointer hover:opacity-75 transition"
+  >
+<Avatar user={currentUser} />
+  </div>
 </nav>
     </div>
   )
--- app/components/sidebar/Sidebar.tsx
+++ app/components/sidebar/Sidebar.tsx
@@ -1,3 +1,4 @@
+import getCurrentUser from "@/app/actions/getCurrentUser";
 import DesktopSidebar from "./DesktopSidebar";
 import MobileFooter from "./MobileFooter";
 
@@ -5,9 +6,10 @@ async function Sidebar ({children} : {
   children: React.ReactNode;
 }) 
 {
+    const currentUser = await getCurrentUser();
     return (
         <div className="h-full">
-            <DesktopSidebar/>
+            <DesktopSidebar currentUser={currentUser!}/>
             <MobileFooter/>
             <main className="lg:pl-20 h-full">
             {children}
