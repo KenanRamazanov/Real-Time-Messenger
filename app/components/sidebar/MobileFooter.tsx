cbdd6ed0fdbfe7edd832c3c4117300134de26f40
kenan551 <kenan.ramazanov1104@gmail.com>
2024-01-04 23:55:55 +0400

    Creating MobileItem

--- app/components/sidebar/MobileFooter.tsx
+++ app/components/sidebar/MobileFooter.tsx
@@ -1,7 +1,40 @@
 "use client"
+
+import useConversation from "@/app/hooks/useConversation";
+import useRoutes from "@/app/hooks/useRoutes"
+import MobileItem from "./MobileItem";
+
 const MobileFooter = () => {
+  const routes = useRoutes();
+  const { isOpen } = useConversation();
+  if (isOpen) {
+    return null;
+  }
   return (
-    <div>MobileFooter</div>
+    <div
+    className="
+    fixed 
+    justify-between 
+    w-full 
+    bottom-0 
+    z-40 
+    flex 
+    items-center 
+    bg-white 
+    border-t-[1px] 
+    lg:hidden
+    "
+    >
+      {routes.map((route) => (
+        <MobileItem 
+          key={route.href} 
+          href={route.href} 
+          active={route.active} 
+          icon={route.icon}
+          onClick={route.onClick}
+        />
+      ))}
+    </div>
   )
 }
 
