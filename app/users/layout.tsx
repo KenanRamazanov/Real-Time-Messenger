1fc5088f42de441a6d39f528c44ff3e3e4359b9a
kenan551 <kenan.ramazanov1104@gmail.com>
2024-01-08 23:46:20 +0400

    Creating UserList

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,5 +1,6 @@
 import getUsers from "../actions/getUsers";
 import Sidebar from "../components/sidebar/Sidebar";
+import UserList from "./components/UserList";
 
 
 export default async function UsersLayout({
