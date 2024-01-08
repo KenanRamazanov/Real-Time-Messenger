e1c66b23d66e7977ea4f9a5c679783cead89a7da
kenan551 <kenan.ramazanov1104@gmail.com>
2024-01-09 00:04:09 +0400

    Adding Functionality to UserBox

--- app/users/components/UserBox.tsx
+++ app/users/components/UserBox.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
-
-const UserBox = () => {
+"use client"
+import {  User } from "@prisma/client";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useCallback, useState } from "react";
+interface UserBoxProps{
+    data : User;
+}
+const UserBox:React.FC<UserBoxProps> = ({
+    data
+}) => {
+    const router = useRouter();
+    const [isLoading,setIsLoading] = useState(false);
+    const handleClick = useCallback(()=> {
+        setIsLoading(true);
+        axios.post('/api/conversations', { userId: data.id })
+        .then((data) => {
+          router.push(`/conversations/${data.data.id}`);
+        })
+        .finally(() => setIsLoading(false));
+    },[data,router]) 
   return (
-    <div>UserBox</div>
+    <div>User</div>
   )
 }
 
