aa2f5d97d1583877ef37ea32becbae21ec3cb29f
kenan551 <kenan.ramazanov1104@gmail.com>
2024-01-09 19:30:12 +0400

    Final of UserBox

--- app/users/components/UserBox.tsx
+++ app/users/components/UserBox.tsx
@@ -1,4 +1,5 @@
 "use client"
+import Avatar from "@/app/components/Avatar";
 import {  User } from "@prisma/client";
 import axios from "axios";
 import { useRouter } from "next/navigation";
@@ -20,7 +21,33 @@ const UserBox:React.FC<UserBoxProps> = ({
         .finally(() => setIsLoading(false));
     },[data,router]) 
   return (
-    <div>User</div>
+    <div 
+    onClick={handleClick}
+    className="
+    w-full 
+    relative 
+    flex 
+    items-center 
+    space-x-3 
+    bg-white 
+    p-3 
+    hover:bg-neutral-100
+    rounded-lg
+    transition
+    cursor-pointer
+    ">
+    <Avatar user={data}/>
+    <div className="min-w-0 flex-1">
+      <div className="focus:outline-none">
+        <div className="flex justify-between items-center mb-1">
+        <p className="text-sm font-medium text-gray-900">
+          {data.name}
+          </p>
+        </div>
+      </div>
+    </div>
+
+    </div>
   )
 }
 
